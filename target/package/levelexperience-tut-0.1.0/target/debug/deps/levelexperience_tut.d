2e601d75f989f878474544a240d1100991ac636e
xRuno <ifagenbruno@proton.me>
2024-05-25 16:43:45 -0300

    fifth

--- target/package/levelexperience-tut-0.1.0/target/debug/deps/levelexperience_tut.d
+++ target/package/levelexperience-tut-0.1.0/target/debug/deps/levelexperience_tut.d
@@ -1,5 +1,5 @@
-C:\Users\Lawnus\Desktop\Mesa-1\Rustlang\levelexperience-tut\target\debug\deps\levelexperience_tut.exe: src\main.rs
+C:\Users\Lawnus\Desktop\Mesa-1\Rustlang\levelexperience-tut\target\package\levelexperience-tut-0.1.0\target\debug\deps\levelexperience_tut.exe: src\main.rs
 
-C:\Users\Lawnus\Desktop\Mesa-1\Rustlang\levelexperience-tut\target\debug\deps\levelexperience_tut.d: src\main.rs
+C:\Users\Lawnus\Desktop\Mesa-1\Rustlang\levelexperience-tut\target\package\levelexperience-tut-0.1.0\target\debug\deps\levelexperience_tut.d: src\main.rs
 
 src\main.rs:
